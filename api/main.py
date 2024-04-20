672554758d553ccdd37000ada3b5b550fc747dce
Reginald Hardwick <reggie19136@gmail.com>
2024-04-20 13:13:48 -0400

    Add custom words function that allows for new word to be added to empty list and cannot have duplicate words

--- api/main.py
+++ api/main.py
@@ -5,3 +5,18 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello":"World"}
+
+customWords = []
+@app.get("/add-custom-word/{word}")
+def add_custom_word(word: str):
+
+    if word in customWords:
+        return {"message": "Word already in the list", "word": word}
+
+    else:
+        customWords.append(word)
+        return{"Added word":word}
+
+@app.get('/custom-words')
+def custom_words():
+    return {"customWords": customWords}
