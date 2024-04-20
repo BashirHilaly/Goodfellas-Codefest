7e7cb9448c8ed1b0a993e122c4c08935f1f3616a
BashirHilaly <bashirkenza@gmail.com>
2024-04-20 13:10:43 -0400

    model imported

--- api/main.py
+++ api/main.py
@@ -1,7 +1,19 @@
+import uvicorn
 from fastapi import FastAPI
+import model
+
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello":"World"}
+
+@app.get('/model')
+def get_model():
+    modelprint = model.model
+    return {modelprint}
+    
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
