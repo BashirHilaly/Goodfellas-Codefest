7edd09c27c26c7ef12efe921e6bf804a4f3f2766
Reginald Hardwick <reggie19136@gmail.com>
2024-04-20 13:37:58 -0400

    Create delete function for custom words

--- api/main.py
+++ api/main.py
@@ -1,7 +1,4 @@
-import uvicorn
 from fastapi import FastAPI
-import model
-
 
 app = FastAPI()
 
@@ -9,11 +6,29 @@ app = FastAPI()
 def read_root():
     return {"Hello":"World"}
 
-@app.get('/model')
-def get_model():
-    modelprint = model.model
-    return {modelprint}
-    
+customWords = []
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+# Add new words to the censor list
+@app.get("/add-custom-word/{word}")
+def add_custom_word(word: str):
+
+    if word in customWords:
+        return {"message": "Word already in the list", "word": word}
+    else:
+        customWords.append(word)
+        return{"Added word":word}
+
+# Delete words from censor list
+@app.get("/delete-custom-word/{word}")
+def delete_custom_word(word: str):
+        if word in customWords:
+             customWords.remove(word)
+             return {"Custom word": "Word removed from the list", "word": word}
+        else:
+             return {"Custom word": "Not found", "word": word}
+
+
+# Check the list of custom words
+@app.get('/custom-words')
+def custom_words():
+    return {"customWords": customWords}
