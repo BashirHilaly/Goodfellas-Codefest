caf0082cc2130073f529b083033da482a78d2c53
sokoruchi100 <harmeyforever5435@gmail.com>
2024-04-20 10:39:20 -0400

    created chrome extension framework

---
+++ extension/contentScript.js
@@ -0,0 +1,11 @@
+(() => {
+  // Variables
+
+  // Async/normal functions defined here
+
+  chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    // Receives messages from the background and edits the content page
+  });
+})();
+
+// Other Utility Functions
