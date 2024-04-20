6cac6c9bc8646e4ae87e89289847f10d577b4a4b
sokoruchi100 <harmeyforever5435@gmail.com>
2024-04-20 13:58:46 -0400

    removed pycache, added gitignore, and config.json functionality

--- api/model.py
+++ api/model.py
@@ -1,15 +1,26 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import json
 
-model = tf.keras.models.load_model('D:/Coding/Goodfellas-Codefest/dataset/toxicity_en.csv')
+MAX_LEN = 196
+
+# Load the configuration for different model path computers
+def load_config():
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+    return config
+
+config = load_config()
+model_path = config['model_path']
+model = tf.keras.models.load_model(model_path)
 
 tokenizer = Tokenizer()
 
 def preprocess_text(text):
     # Tokenize and pad the input text
     sequence = tokenizer.texts_to_sequences([text])
-    padded_sequence = pad_sequences(sequence, maxlen=maxlen, padding='post')
+    padded_sequence = pad_sequences(sequence, maxlen=MAX_LEN, padding='post')
     print(padded_sequence)
     return padded_sequence
 
